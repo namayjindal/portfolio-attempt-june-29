63d8de700c314868e5c0391eea4da9a52fc40fb5
Namay Jindal <namayjindal@nyu.edu>
2024-06-29 00:55:36 +0530

    fixed some things

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const greetingElement = document.getElementById('greeting');
-    const greetings = ["hi, i'm namay.", "नमस्ते, मैं नमय हूँ"];
+    const element = document.getElementById('typing-animation');
+    const text = "hi, i'm namay.";
     let index = 0;
+    let isDeleting = false;
+    const typingSpeed = 150;
+    const deletingSpeed = 100;
+    const delay = 2000; // 2 seconds delay at the end of typing
 
-    setInterval(() => {
-        index = (index + 1) % greetings.length;
-        greetingElement.textContent = greetings[index];
-    }, 10000);
+    function type() {
+        element.textContent = text.slice(0, index);
+
+        if (!isDeleting && index < text.length) {
+            index++;
+            setTimeout(type, typingSpeed);
+        } else if (isDeleting && index > 0) {
+            index--;
+            setTimeout(type, deletingSpeed);
+        } else if (index === text.length) {
+            isDeleting = true;
+            setTimeout(type, delay);
+        } else if (index === 0) {
+            isDeleting = false;
+            setTimeout(type, typingSpeed);
+        }
+    }
+
+    type();
 });
